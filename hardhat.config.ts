b593b6a171bd81ef9bb3a6789b40788858a654ca
sujine2 <twinsujin22@gmail.com>
2023-07-16 10:32:57 +0900

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,75 +1,43 @@
-import '@nomiclabs/hardhat-waffle'
-import '@typechain/hardhat'
-import { HardhatUserConfig } from 'hardhat/config'
-import 'hardhat-deploy'
-import '@nomiclabs/hardhat-etherscan'
-
-import 'solidity-coverage'
-
-import * as fs from 'fs'
-
-const mnemonicFileName = process.env.MNEMONIC_FILE ?? `${process.env.HOME}/.secret/testnet-mnemonic.txt`
-let mnemonic = 'test '.repeat(11) + 'junk'
-if (fs.existsSync(mnemonicFileName)) { mnemonic = fs.readFileSync(mnemonicFileName, 'ascii') }
-
-function getNetwork1 (url: string): { url: string, accounts: { mnemonic: string } } {
-  return {
-    url,
-    accounts: { mnemonic }
-  }
-}
-
-function getNetwork (name: string): { url: string, accounts: { mnemonic: string } } {
-  return getNetwork1(`https://${name}.infura.io/v3/${process.env.INFURA_ID}`)
-  // return getNetwork1(`wss://${name}.infura.io/ws/v3/${process.env.INFURA_ID}`)
-}
-
-const optimizedComilerSettings = {
-  version: '0.8.17',
-  settings: {
-    optimizer: { enabled: true, runs: 1000000 },
-    viaIR: true
-  }
-}
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+import { HardhatUserConfig } from "hardhat/config";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-deploy";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [{
-      version: '0.8.15',
-      settings: {
-        optimizer: { enabled: true, runs: 1000000 }
-      }
-    }],
-    overrides: {
-      'contracts/core/EntryPoint.sol': optimizedComilerSettings,
-      'contracts/samples/SimpleAccount.sol': optimizedComilerSettings
-    }
+  namedAccounts: {
+    deployer: 0,
+    withdrawer: 1,
+    user: 2,
   },
   networks: {
-    dev: { url: 'http://localhost:8545' },
-    // github action starts localgeth service, for gas calculations
-    localgeth: { url: 'http://localgeth:8545' },
-    goerli: getNetwork('goerli'),
-    sepolia: getNetwork('sepolia'),
-    proxy: getNetwork1('http://localhost:8545')
+    development: {
+      url: "http://127.0.0.1:8545",
+      allowUnlimitedContractSize: true,
+    },
+    goerli: {
+      url: process.env.INFURA_KEY,
+      accounts: [process.env.DEPLOYER!, process.env.USER1!, process.env.USER2!],
+      chainId: 5,
+    },
+    hardhat: {
+      blockGasLimit: 12000000,
+    },
   },
-  mocha: {
-    timeout: 10000
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      outputSelection: {
+        "*": {
+          "*": ["storageLayout"],
+        },
+      },
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+    },
   },
+};
 
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
-
-}
-
-// coverage chokes on the "compilers" settings
-if (process.env.COVERAGE != null) {
-  // @ts-ignore
-  config.solidity = config.solidity.compilers[0]
-}
-
-export default config
+export default config;
