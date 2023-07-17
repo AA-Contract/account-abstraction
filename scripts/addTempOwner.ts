228cc320f9d0965f8ec9bfc20ac12e8dd1bbdb47
healthykim <bsbs8645@snu.ac.kr>
2023-07-18 00:12:35 +0900

    fix: Fix add temp owner error

--- scripts/addTempOwner.ts
+++ scripts/addTempOwner.ts
@@ -1,41 +1,41 @@
-import { ethers } from 'hardhat'
-import { TestExpiryAccount } from '../typechain'
-import { Wallet } from 'ethers';
+import { ethers } from "hardhat";
+import { TestExpiryAccount } from "../typechain";
+import { Wallet } from "ethers";
 
 async function addTempOwner(
-    account: TestExpiryAccount,
-    signer: Wallet,
-    validAfter: number,
-    validUntil: number
+  account: TestExpiryAccount,
+  signer: Wallet,
+  validAfter: number,
+  validUntil: number
 ) {
-    /*
+  /*
     ========Add session key to wallet with given time range========
     */
 
-    const owner = (await ethers.getSigners())[0]
-    // Used TestCounter Contract as a target method
-    const counter = await ethers.getContractAt("TestCounter", "0x3918E00482b2C5F3f25beD0981a694f64B2281DE")
-    const count = counter.interface.encodeFunctionData('count')
-    const targetMethods : TestExpiryAccount.TargetMethodsStruct[] = [
-        {
-            delegatedContract: counter.address,
-            delegatedFunctions: [
-            count 
-            ]
-        }
-    ];
-    
-    const tx = await account.connect(owner).addTemporaryOwner(
-      signer.address, 
-      validAfter, 
-      validUntil, 
-      targetMethods,
-      {
-        gasPrice: 5e5
-      }
-    )
-    await tx.wait()
-    console.log(`Added ${signer.address} as a temporary owner, tx hash is:`, tx.hash)
+  // const owner = (await ethers.getSigners())[0];
+  const owner = new ethers.Wallet(
+    process.env.DEPLOYER_SESSION!,
+    ethers.provider
+  );
+
+  // Used TestCounter Contract as a target method
+  const counter = await ethers.getContractAt(
+    "TestCounter",
+    "0x3918E00482b2C5F3f25beD0981a694f64B2281DE"
+  );
+  const count = counter.interface.encodeFunctionData("count");
+  const targetMethods: TestExpiryAccount.TargetMethodsStruct[] = [
+    {
+      delegatedContract: counter.address,
+      delegatedFunctions: [count],
+    },
+  ];
+
+  const tx = await account
+    .connect(owner)
+    .addTemporaryOwner(signer.address, validAfter, validUntil, targetMethods);
+  await tx.wait();
+  console.log(`Added ${signer.address}\ntx hash is:`, tx.hash);
 }
 
 export default addTempOwner;
--- scripts/test_main.ts
+++ scripts/test_main.ts
@@ -1,80 +1,128 @@
 // To get full UserOp from given TestExpiryAccount and tempOwner
-import { ethers } from 'hardhat'
-import addTempOwner from './addTempOwner';
-import sign from './signUserOp';
+import { ethers } from "hardhat";
+import addTempOwner from "./addTempOwner";
+import sign from "./signUserOp";
 
+function padTo2Digits(num: number) {
+  return num.toString().padStart(2, "0");
+}
+
+// 👇️ format as "YYYY-MM-DD hh:mm:ss"
+// You can tweak the format easily
+function formatDate(ms: number) {
+  const date = new Date(ms);
+  return (
+    [
+      date.getFullYear(),
+      padTo2Digits(date.getMonth() + 1),
+      padTo2Digits(date.getDate()),
+    ].join("-") +
+    " " +
+    [
+      padTo2Digits(date.getHours()),
+      padTo2Digits(date.getMinutes()),
+      padTo2Digits(date.getSeconds()),
+    ].join(":")
+  );
+}
 async function main() {
-    // Hardcoded TestExpiryAccount address
-    const account = await ethers.getContractAt(
-        'TestExpiryAccount', 
-        "0x3eB48129c1B3E2f8022C4177a8329c7DFEf45f99"
-    )
-    // Global Entrypoint contract for goerli testnet
-    const entryPointAddr = "0x7eb6D1C6a5C0c30b97668FC391EC9f0e5250a816"
-    const entryPoint = await ethers.getContractAt('EntryPoint', entryPointAddr)
-    
-    // Owner of TestExpiryAccount
-    const owner = (await ethers.getSigners())[0]
-    // Session Key Owners
-    const signer_1 = new ethers.Wallet(`${process.env.TEST_ACCOUNT}`)
-    const signer_2 = new ethers.Wallet(`${process.env.TEST_ACCOUNT_2}`)
-    
-    /*
+  console.log("*********************************************\n");
+  console.log("            ⏳  Session Key Test  ⏳          ");
+  console.log("\n*********************************************\n");
+  // Hardcoded TestExpiryAccount address
+  const account = await ethers.getContractAt(
+    "TestExpiryAccount",
+    "0x3eB48129c1B3E2f8022C4177a8329c7DFEf45f99"
+  );
+  // Global Entrypoint contract for goerli testnet
+  const entryPointAddr = "0x7eb6D1C6a5C0c30b97668FC391EC9f0e5250a816";
+  const entryPoint = await ethers.getContractAt("EntryPoint", entryPointAddr);
+
+  // Owner of TestExpiryAccount
+  const owner = (await ethers.getSigners())[0];
+  // Session Key Owners
+  const signer_1 = new ethers.Wallet(`${process.env.TEST_ACCOUNT}`);
+  const signer_2 = new ethers.Wallet(`${process.env.TEST_ACCOUNT_2}`);
+
+  /*
     ==============Calling addTemporaryOwner() Function==============
     */
-    
-    
-    //@dev Only needed at the first time, now commented out
-    
-    // console.log('Add temporary owner...')
-    // let now = await ethers.provider.getBlock('latest').then(block => block.timestamp)
 
-    // await addTempOwner(
-    //   account,
-    //   signer_1,
-    //   now - 2000,
-    //   now + 10000000 // Around November, 2023
-    // )
-    
-    // await addTempOwner(
-    //   account, 
-    //   signer_2, 
-    //   now - 2000, 
-    //   now + 10000000 // Around November, 2023
-    // )
+  //@dev Only needed at the first time, now commented out
 
+  console.log("\n        [Add Temporary Owner]        ");
+  let now = await ethers.provider
+    .getBlock("latest")
+    .then((block) => block.timestamp);
 
-    /*
-    ==============Signing UserOp==============
+  console.log("\nAdd temporary owner... ('count' method)");
+
+  await addTempOwner(
+    account,
+    signer_1,
+    now - 2000,
+    now + 10000000 // Around November, 2023
+  );
+
+  await addTempOwner(
+    account,
+    signer_2,
+    now - 2000,
+    now + 10000000 // Around November, 2023
+  );
+
+  console.log("\nOwner List");
+  console.log(
+    `1. ${signer_1.address} : ${formatDate(now - 2000)} ~ ${formatDate(
+      now + 10000000
+    )}`
+  );
+  console.log(
+    `2. ${signer_2.address} : ${formatDate(now - 2000)} ~ ${formatDate(
+      now + 10000000
+    )}`
+  );
+
+  /*
+    ==============Send Count Transaction==============
     */
-    
-    console.log(`Testing UserOp signed by ${signer_1.address} and ${signer_2.address}...`)
-    const successOp = await sign(account, signer_1, entryPoint, true)
-    const failOp = await sign(account, signer_2, entryPoint, false)
-    console.log('First user operation will succeed:', successOp)
-    console.log('Second user operation will fail:', failOp)
 
+  console.log("\n        [Test Temporary Owner: send 'count' tx]        ");
+  const successOp = await sign(account, signer_1, entryPoint, true);
 
-    /*
+  console.log("\nSend transaction to counter contract...");
+
+  const tx1 = await entryPoint.handleOps([successOp], owner.address, {
+    gasLimit: 10e6,
+  });
+
+  const failOp = await sign(account, signer_2, entryPoint, false);
+  console.log("First user operation will succeed:", successOp);
+  console.log("Second user operation will fail:", failOp);
+
+  /*
     ==============Calling handleOps() through entrypoint==============
     */
-    console.log("Calling handleOps() through entrypoint...")
+  console.log("Calling handleOps() through entrypoint...");
 
-    const tx1 = await entryPoint.handleOps([successOp], owner.address, {gasLimit: 10e6})
-    await tx1.wait()
-    console.log("Executing the first UserOp is completed at:", tx1.hash)
+  // const tx1 = await entryPoint.handleOps([successOp], owner.address, {
+  //   gasLimit: 10e6,
+  // });
+  await tx1.wait();
+  console.log("Executing the first UserOp is completed at:", tx1.hash);
 
-    // Will revert at this line
-    // staticcall to see the specific revert reason
-    await entryPoint.callStatic.handleOps([failOp], owner.address, {gasLimit: 10e6})
-      .catch(error => {
-          console.log("Transaction failed. Reverted reason:", error.errorArgs[1])
-      })
+  // Will revert at this line
+  // staticcall to see the specific revert reason
+  await entryPoint.callStatic
+    .handleOps([failOp], owner.address, { gasLimit: 10e6 })
+    .catch((error) => {
+      console.log("Transaction failed. Reverted reason:", error.errorArgs[1]);
+    });
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
